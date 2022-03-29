bc1ee69767bc8688528372b215759b1f4711d956
weakryan <99753350+weakryan@users.noreply.github.com>
2022-03-29 15:52:58 -0300

    Add files via upload

---
+++ Exercicio7.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Exercicio7 {
+  
+            public static void main(String[] args){
+
+                    float premio;
+                premio = 780.000f;
+                float remuneracao1;
+                remuneracao1 = (46 * premio) / 100;
+                System.out.println("O primeiro ganhador ficará com " + remuneracao1 + " Reais");
+                float remuneracao2;
+                remuneracao2 = (32 * premio) / 100;
+                System.out.println("O segundo ganhador ficará com " + remuneracao2 + " Reais");
+                float remuneracao3; 
+                remuneracao3 = (22 * premio) / 100;
+                System.out.println("O terceiro ganhador ficará com " + remuneracao3 + " Reais");
+            
+            }
+          
+    }
+
+    
+    
